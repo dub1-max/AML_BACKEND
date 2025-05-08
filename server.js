eb2a7f9039f3a7bb2fb96dbc98b1504a52783be0
RaulTheMiner17 <raulferns1@gmail..com>
2025-05-08 15:23:37 +0530

    tracking fix?

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mysql = require('mysql2/promise');
 const cors = require('cors');
 const Papa = require('papaparse');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const session = require('express-session');
 require('dotenv').config();
 
